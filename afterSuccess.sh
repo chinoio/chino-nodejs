f9b642bbc022a864901075e717be5131b8356ee4
danibix95 <dani95bissoli@gmail.com>
2017-03-16 14:10:29 +0100

    Fix oauth token usage.

--- afterSuccess.sh
+++ afterSuccess.sh
@@ -8,13 +8,13 @@ then
     npm run generate_docs
     git config --global user.email "danibix95@users.noreply.github.com"
     git config --global user.name "travis-ci"
-    git clone --quiet --branch=gh-pages https://${GH_TOKEN}@https://github.com/danibix95/chino-nodejs.git gh-pages
+    git clone --quiet --branch=gh-pages https://github.com/danibix95/chino-nodejs.git gh-pages
     if cd gh-pages; then
         git rm -rf .
         cp -r ../docs/*/* .
         git add -A
         git commit -m "Documentation updated by Travis CI (build $TRAVIS_BUILD_NUMBER)"
-        git push --quiet https://${GH_TOKEN}@github.com/danibix95/chino-nodejs.git gh-pages
+        git push --quiet https://${GH_PAGES}@github.com/danibix95/chino-nodejs.git gh-pages
         cd ..
     fi
 fi
